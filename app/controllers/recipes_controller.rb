84d86a5e8e76adbd637ee1397fe61a53cbed8c2b
Josh Hawkins <hawkins@users.noreply.github.com>
2018-12-31 11:48:38 -0600

    High-low selection strategy

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -79,12 +79,44 @@ class RecipesController < ApplicationController
   end
 
   def generate_tag_color(tag)
+    # Short-circuit custom tag names
+    specific = get_specific_tag_color tag
+    return specific unless specific.nil?
+
+    # Randomly generate a color using a custom high-low selection strategy
     seed = tag.bytes.map(&:to_i).reduce(0, :+)
     r = Random.new(seed)
-    red = (r.rand * 255).to_i
-    green = (r.rand * 255).to_i
-    blue = (r.rand * 255).to_i
-    "#%02X%02X%02X" % [red, green, blue]
+    a = (r.rand * 64).to_i * 4
+    b = (r.rand * 64).to_i * 4
+
+    # TODO: DRY
+    if a >= 192 && b >= 192
+      # high high
+      c = color_low(r)
+    elsif (a >= 192 && b >= 64 && b < 192) || (a >= 64 && a < 192 && b >= 192)
+      # high medium
+      c = color_random(r)
+    elsif (a >= 192 && b < 64) || (a < 64 && b >= 192)
+      # high low
+      c = color_high_low(r)
+    elsif a >= 64 && a < 192 && b >= 64 && b < 192
+      # middle middle
+      c = color_high_low(r)
+    elsif (a >= 64 && a < 192 && b < 64) || (a < 64 && b >= 64 && b < 192)
+      # middle low
+      c = color_random(r)
+    else
+      # low low
+      c = color_high(r)
+    end
+
+    # Decide order
+
+    d = r.rand * 3
+    colors = [a, b, c]
+    colors.shuffle!(random: r)
+
+    format('#%02X%02X%02X', colors[0], colors[1], colors[2])
   end
   helper_method :generate_tag_color
 
@@ -109,5 +141,39 @@ class RecipesController < ApplicationController
                   :user_id,
                   :tags,
                   :source)
+  end
+
+  def get_specific_tag_color(tag)
+    case tag.downcase
+    when 'christmas'
+      'yellowgreen'
+    when 'easy'
+      'limegreen'
+    when 'medium'
+      'orange'
+    when 'hard'
+      'red'
+    end
+  end
+
+  def color_high(random)
+    (random.rand * 64) + 192
+  end
+
+  def color_middle(random)
+    (random.rand * 64) + 128
+  end
+
+  def color_low(random)
+    random.rand * 64
+  end
+
+  def color_high_low(random)
+    high_bit = (random.rand + 0.5).to_i
+    random.rand * 64 + (128 * high_bit)
+  end
+
+  def color_random(random)
+    random.rand * 256
   end
 end
