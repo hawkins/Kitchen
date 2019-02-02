66f73486ce390e5ccfac57476c84ffe7e33fa7ea
Josh Hawkins <hawkins@users.noreply.github.com>
2019-02-02 09:13:54 -0500

    Permit name
    Should fix #2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    registration_params = [:name, :email, :password, :password_confirmation, :commit]
+
+    if params[:action] == 'update'
+      registration_params << :current_password
+      devise_parameter_sanitizer.permit(:account_update, keys: registration_params)
+    elsif params[:action] == 'create'
+      devise_parameter_sanitizer.permit(:sign_up, keys: registration_params)
+    end
+  end
 end
